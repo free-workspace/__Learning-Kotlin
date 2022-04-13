e7bacc6708470dcaf56be598217b3ca07e7c7570
be愛tri√.ᴘʏ $ <84352260+beahtrwzll@users.noreply.github.com>
2022-04-13 14:39:24 -0300

    Upload ByteBank Project

---
+++ ByteBank (Alura Project)/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    // "val" para declarar constantes:
+    val welcome = "Bem-vindo ao Bytebank"
+    println(welcome) // O "println()" faz uma quebra de linha.
+
+    // "var" para declarar variáveis:
+    val holder = "Beatriz"
+    print("Titular: $holder") // O "print()" não faz quebra de linha.
+
+    val account = 3957285
+
+    var balance = 0.0
+}
